fcb6c176330b52cab208bd9f5b2f91ee959b0a50
Pobx <geidtiphong.singseewo@krungsrinimble.com>
2022-11-05 11:15:49 +0700

    feat: change namespace

---
+++ ContractsBook/ContractsBook/ViewModels/ContactViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using ContractsBook.Models;
+using Xamarin.Forms;
+
+namespace ContractsBook.ViewModels
+{
+    public class ContactViewModel : BaseViewModel
+    {
+        public int Id { get; set; }
+        private string _firstName;
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                SetValue(ref _firstName, value);
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                SetValue(ref _lastName, value);
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                SetValue(ref _email, value);
+            }
+        }
+
+        private string _phone;
+        public string Phone
+        {
+            get { return _phone; }
+            set
+            {
+                SetValue(ref _phone, value);
+            }
+        }
+
+        private bool _isFavorite;
+        public bool IsFavorite
+        {
+            get { return _isFavorite; }
+            set
+            {
+                SetValue(ref _isFavorite, value);
+                OnPropertyChanged(nameof(ProfileImage));
+            }
+        }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+        public ImageSource ProfileImage
+        {
+            get
+            {
+                if (IsFavorite)
+                {
+                    return ImageSource.FromResource("ContactsBook.Assets.Images.FavoriteProfile.png");
+                }
+
+                return ImageSource.FromResource("ContactsBook.Assets.Images.NormalProfile.png");
+            }
+        }
+
+        public ContactViewModel()
+        {
+        }
+
+        public ContactViewModel(Contact contact)
+        {
+            Id = contact.Id;
+        }
+    }
+}
+
