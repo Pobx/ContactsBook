da3823308eeef6ac4b7fed84d5a1cdad9f2b50ce
Pobx <geidtiphong.singseewo@krungsrinimble.com>
2022-11-05 11:52:56 +0700

    feat: implement SQLiteContactStore

---
+++ ContractsBook/ContractsBook/Persistence/SQLiteContactStore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ContractsBook.Models;
+using ContractsBook.ViewModels;
+using SQLite;
+
+namespace ContractsBook.Persistence
+{
+    public class SQLiteContactStore : IContactStore
+    {
+        private SQLiteAsyncConnection _connection;
+
+        public SQLiteContactStore(ISQLiteDb db)
+        {
+            _connection = db.Getconnection();
+            _connection.CreateTableAsync<Contact>();
+        }
+
+        public async Task AddContact(Contact contact)
+        {
+            await _connection.InsertAsync(contact);
+        }
+
+        public async Task DeleteContact(Contact contact)
+        {
+            await _connection.DeleteAsync(contact);
+        }
+
+        public async Task<Contact> GetContact(int id)
+        {
+            return await _connection.FindAsync<Contact>(id);
+        }
+
+        public async Task<IEnumerable<Contact>> GetContactsAsync()
+        {
+            return await _connection.Table<Contact>().ToListAsync();
+        }
+
+        public async Task UpdateContact(Contact contact)
+        {
+            await _connection.UpdateAsync(contact);
+        }
+    }
+}
+
---
+++ ContractsBook/ContractsBook/ViewModels/IContactStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ContractsBook.Models;
+
+namespace ContractsBook.ViewModels
+{
+    public interface IContactStore
+    {
+        Task<IEnumerable<Contact>> GetContactsAsync();
+        Task<Contact> GetContact(int id);
+        Task AddContact(Contact contact);
+        Task UpdateContact(Contact contact);
+        Task DeleteContact(Contact contact);
+    }
+}
+
