57fcd17ae9e030b1e967187ac970c45a74306f42
Pobx <geidtiphong.singseewo@krungsrinimble.com>
2022-11-05 12:35:41 +0700

    feat: create new one

---
+++ ContractsBook/ContractsBook/Views/ContactsDetailPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using ContractsBook.ViewModels;
+using Xamarin.Forms;
+
+namespace ContractsBook.Views
+{
+    public partial class ContactsDetailPage : ContentPage
+    {
+        public ContactsDetailPage(ContactViewModel viewModel)
+        {
+            InitializeComponent();
+        }
+    }
+}
+
